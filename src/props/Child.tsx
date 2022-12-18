492d97a0ea6b1961be6809735da48c2b2aeb0de5
Mihai Smarandache <mihai.sanfran@gmail.com>
2022-12-17 19:17:33 -0700

    Add onClick function

--- src/props/Child.tsx
+++ src/props/Child.tsx
@@ -5,13 +5,22 @@ interface ChildProps {
 }
 
 // destructure color props from ChildProps interface
-export const Child = ({ color }: ChildProps) => {
-  return <div>Child component! {color}</div>;
+export const Child = ({ color, onClick }: ChildProps) => {
+  return (
+    <div>
+      Child component! {color}
+      <button onClick={onClick}>Click me</button>
+    </div>
+  );
 };
 
 // proper way to define a function component with typescript
 // tells typescript it is a React component which uses the ChildProps interface
 // FC - function component
-export const ChildAsFC: React.FC<ChildProps> = ({ color }) => {
-  return <div>{color}</div>;
+export const ChildAsFC: React.FC<ChildProps> = ({ color, onClick }) => {
+  return (
+    <div>
+      {color} <button onClick={onClick}>Click me</button>
+    </div>
+  );
 };
