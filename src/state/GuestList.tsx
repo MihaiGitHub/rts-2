c75b2a42bd342b5122855cfc89e0405cfdbdd94d
Mihai Smarandache <mihai.sanfran@gmail.com>
2022-12-18 15:13:51 -0700

    Create GuestList component

---
+++ src/state/GuestList.tsx
@@ -0,0 +1,6 @@
+// no angle brackets because not expecting any props; interface only used when props received
+const GuestList: React.FC = () => {
+  return <div>Guest List</div>;
+};
+
+export default GuestList;
