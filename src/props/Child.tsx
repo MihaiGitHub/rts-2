6cf899cbc429854ad15e65ffcf543e6298ff63d8
Mihai Smarandache <mihai.sanfran@gmail.com>
2022-12-17 18:56:41 -0700

    Define new React component

--- src/props/Child.tsx
+++ src/props/Child.tsx
@@ -6,3 +6,9 @@ interface ChildProps {
 export const Child = ({ color }: ChildProps) => {
   return <div>Child component! {color}</div>;
 };
+
+// proper way to define a function component with typescript
+// tells typescript it is a React component which uses the ChildProps interface
+export const ChildAsFC: React.FC<ChildProps> = ({ color }) => {
+  return <div>{color}</div>;
+};
