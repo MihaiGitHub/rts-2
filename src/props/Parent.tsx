4d8dd1b900e86afeb1c0f2cbc20f7486a0d3479d
Mihai Smarandache <mihai.sanfran@gmail.com>
2022-12-17 19:20:57 -0700

    Pass children props

--- src/props/Parent.tsx
+++ src/props/Parent.tsx
@@ -1,7 +1,11 @@
 import { Child } from "./Child";
 
 const Parent = () => {
-  return <Child color="red" onClick={() => console.log("clicked")} />;
+  return (
+    <Child color="red" onClick={() => console.log("clicked")}>
+      Childre prop
+    </Child>
+  );
 };
 
 export default Parent;
