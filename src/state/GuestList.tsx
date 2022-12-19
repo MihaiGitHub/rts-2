a5c9f4aa4e18a303ee9c09963a670cf7b0804bf2
Mihai Smarandache <mihai.sanfran@gmail.com>
2022-12-18 17:07:27 -0700

    Create onClick event for guests

--- src/state/GuestList.tsx
+++ src/state/GuestList.tsx
@@ -3,12 +3,19 @@ import { useState } from "react";
 // no angle brackets because not expecting any props; interface only used when props received
 const GuestList: React.FC = () => {
   const [name, setName] = useState("");
+  // need to tell typescript what type of array this is
+  const [guests, setGuests] = useState<string[]>([]);
+
+  const onClick = () => {
+    setName("");
+    setGuests([...guests, name]);
+  };
 
   return (
     <div>
       <h3>Guest List</h3>
       <input value={name} onChange={(e) => setName(e.target.value)} />
-      <button>Add Guest</button>
+      <button onClick={onClick}>Add Guest</button>
     </div>
   );
 };
