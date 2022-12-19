45018f541beb4153582248e8707a43d48a82d6aa
Mihai Smarandache <mihai.sanfran@gmail.com>
2022-12-18 17:34:39 -0700

    Create users array

--- src/state/UserSearch.tsx
+++ src/state/UserSearch.tsx
@@ -1,5 +1,11 @@
 import { useState } from "react";
 
+const users = [
+  { name: "Sarah", age: 20 },
+  { name: "Alex", age: 21 },
+  { name: "Michael", age: 22 },
+];
+
 const UserSearch: React.FC = () => {
   return <div>User Search</div>;
 };
