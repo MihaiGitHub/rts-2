5165a6afb0d2f1c2400733af9bd567d0bc89c536
Mihai Smarandache <mihai.sanfran@gmail.com>
2022-12-18 17:31:48 -0700

    Create and display UserSearch component

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,13 @@
 import ReactDOM from "react-dom";
 import GuestList from "./state/GuestList";
+import UserSearch from "./state/UserSearch";
 
 const App = () => {
   return (
     <div>
       <h1>App is working!</h1>
       <div>
+        <UserSearch />
         <GuestList />
       </div>
     </div>
