f70e37e31b31384607c5b4f8f5aac81f2b0a6f2d
Mihai Smarandache <mihai.sanfran@gmail.com>
2022-12-18 17:57:35 -0700

    Create search user function

--- src/state/UserSearch.tsx
+++ src/state/UserSearch.tsx
@@ -7,7 +7,21 @@ const users = [
 ];
 
 const UserSearch: React.FC = () => {
-  return <div>User Search</div>;
+  const [name, setName] = useState("");
+
+  const onClick = () => {
+    const foundUser = users.find((user) => {
+      return user.name === name;
+    });
+  };
+
+  return (
+    <div>
+      User Search
+      <input value={name} onChange={(e) => setName(e.target.value)} />
+      <button onClick={onClick}> Find User</button>
+    </div>
+  );
 };
 
 export default UserSearch;
